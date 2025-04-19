31010f022b35ac68ec19781f8b5c19653809b739
ST10260322 <ST10260322@vcconnect.edu.za>
2025-04-19 05:21:32 +0200

    Database is functioning

--- app/src/main/java/com/fake/st10262898_budgetbunny_poepart2/UserViewModel.kt
+++ app/src/main/java/com/fake/st10262898_budgetbunny_poepart2/UserViewModel.kt
@@ -2,6 +2,7 @@ package com.fake.st10262898_budgetbunny_poepart2
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -11,12 +12,23 @@ class UserViewModel(application: Application) : AndroidViewModel(application)
     private val userDao = BudgetBunnyDatabase.getDatabase(application).userDao()
     private val repository = UserRepository(userDao)
 
+    //This variable is going to be used to be able to check the registration result:
+    val registrationResult = MutableLiveData<Boolean>()
 
     fun registerUser(username: String, password: String)
     {
         val user = User(username = username, password = password)
+
+
         viewModelScope.launch(Dispatchers.IO) {
-            repository.insert(user)
+            try{
+                repository.insert(user)
+                registrationResult.postValue(true)
+            }
+            catch (e: Exception)
+            {
+                registrationResult.postValue(false)
+            }
         }
     }
 
--- app/src/main/java/com/fake/st10262898_budgetbunny_poepart2/signUpPage.kt
+++ app/src/main/java/com/fake/st10262898_budgetbunny_poepart2/signUpPage.kt
@@ -1,20 +1,54 @@
 package com.fake.st10262898_budgetbunny_poepart2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.collection.emptyLongSet
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
 
 class signUpPage : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        //Declaring the variables so we can add functionality to them:
+        val et_username = findViewById<EditText>(R.id.et_username)
+        val et_password = findViewById<EditText>(R.id.et_password)
+        val btn_signUp = findViewById<Button>(R.id.btn_signUp)
+        val userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+
+        //This method now will add the functionality to the button (therefore saving details of user on database):
+        btn_signUp.setOnClickListener {
+            val username = et_username.text.toString()
+            val password = et_password.text.toString()
+
+            if (username.isNotEmpty() && password.isNotEmpty()) {
+                userViewModel.registerUser(username, password)
+
+                //observe for the result:
+                userViewModel.registrationResult.observe(this, Observer<Boolean> { isSuccess ->
+                    if (isSuccess) {
+                        Toast.makeText(this, "Sign up successful", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Sign up failed", Toast.LENGTH_SHORT).show()
+                    }
+                })
+            } else {
+                Toast.makeText(this, "PLease fill in all the required fields", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
         }
     }
 }
