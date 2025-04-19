c382b74d3a30b162bb3ffa80a44147308bf4b452
ST10260322 <ST10260322@vcconnect.edu.za>
2025-04-19 03:26:58 +0200

    When user now clicks sign-up they go to the Sign-Up page

--- app/src/main/java/com/fake/st10262898_budgetbunny_poepart2/MainActivity.kt
+++ app/src/main/java/com/fake/st10262898_budgetbunny_poepart2/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.fake.st10262898_budgetbunny_poepart2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btn_signUp = findViewById<ImageButton>(R.id.SignUp)
+        btn_signUp.setOnClickListener {
+            val intent = Intent(this, signUpPage::class.java)
+            startActivity(intent)
         }
     }
 }
