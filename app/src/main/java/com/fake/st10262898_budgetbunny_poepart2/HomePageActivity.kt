ba094c9c32135146f34aaba3671e44b29b4b39a4
ST10260322 <ST10260322@vcconnect.edu.za>
2025-04-24 14:44:19 +0200

    Started working on the look of the homePage

---
+++ app/src/main/java/com/fake/st10262898_budgetbunny_poepart2/HomePageActivity.kt
@@ -0,0 +1,22 @@
+package com.fake.st10262898_budgetbunny_poepart2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomePageActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_page)
+
+
+
+
+    }
+}
