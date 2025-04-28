890f4dead1e3c4dd51c2bc1ededabf30bfe82fcf
ST10317620-Frazer <129761537+ST10317620-Frazer@users.noreply.github.com>
2025-04-28 14:40:54 +0200

    sign up reward page with the 50 coins

---
+++ app/src/main/java/com/fake/st10262898_budgetbunny_poepart2/SignUpReward.kt
@@ -0,0 +1,29 @@
+package com.fake.st10262898_budgetbunny_poepart2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignUpReward : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_up_reward)
+
+
+        val closeButton=findViewById<Button>(R.id.closeButton)
+        closeButton.setOnClickListener{
+            finish()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
