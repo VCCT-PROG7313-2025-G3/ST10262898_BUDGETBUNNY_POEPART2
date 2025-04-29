6bc3ad56a85af29fab9a7d5e40dffb7276fe16ba
ST10260322 <ST10260322@vcconnect.edu.za>
2025-04-29 20:27:22 +0200

    A bit of back-end done

--- app/src/main/java/com/fake/st10262898_budgetbunny_poepart2/BunnyNameActivity.kt
+++ app/src/main/java/com/fake/st10262898_budgetbunny_poepart2/BunnyNameActivity.kt
@@ -1,10 +1,10 @@
 package com.fake.st10262898_budgetbunny_poepart2
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class BunnyNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +12,9 @@ class BunnyNameActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_bunny_name)
 
+
+
+        //Find the edit text which contains the bunny's name:
+        val bunnyName = findViewById<EditText>(R.id.bunnyName)
     }
 }
