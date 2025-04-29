e060538331d8e889689c55f84076d2747bf1583f
ST10260322 <ST10260322@vcconnect.edu.za>
2025-04-29 20:25:02 +0200

    The front end of the name the bunny page is complete.

---
+++ app/src/main/java/com/fake/st10262898_budgetbunny_poepart2/BunnyNameActivity.kt
@@ -0,0 +1,16 @@
+package com.fake.st10262898_budgetbunny_poepart2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BunnyNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_bunny_name)
+
+    }
+}
