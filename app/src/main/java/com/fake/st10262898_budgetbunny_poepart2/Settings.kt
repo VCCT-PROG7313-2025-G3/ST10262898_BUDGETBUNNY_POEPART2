08593580b707e9301f9866591451120ee3f607d4
Cailey Mocktar <st10262898@vcconnect.edu.za>
2025-04-30 15:08:40 +0200

    init commit

--- app/src/main/java/com/fake/st10262898_budgetbunny_poepart2/Settings.kt
+++ app/src/main/java/com/fake/st10262898_budgetbunny_poepart2/Settings.kt
@@ -1,20 +1,34 @@
 package com.fake.st10262898_budgetbunny_poepart2
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class Settings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        // Try this explicit initialization
+        val tvDateTime: TextView = findViewById(R.id.tv_dateTime)
+
+        // Set the date and time
+        val currentDateTime = SimpleDateFormat("dd MMMM yyyy, HH:mm", Locale.getDefault()).format(Date())
+        tvDateTime.text = currentDateTime
+
     }
 }
